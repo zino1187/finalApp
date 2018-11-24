4e964dcef8db9cec87c2590f18de2bcdfb5f9e04
koitt-04-P-PC\koitt-04-P <zino1187@gmail.com>
2018-11-24 12:28:53 +0900

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,16 @@ export class AppComponent {
       job:_job
     }).subscribe(data =>{
       //이따가 서버에서 전송된 결과를 처리...
+      //typescript 는 자료형이 존재한다..따라서 data의 자료형을 
+      //알아야 함...Object 형임..
+      console.log(data);
+      //pase의 대상이 되려면 string 이어야 하는데 data는 오브젝트형
+      //이므로 string으로 변환을 먼저 해준후에 parse 시키자!!
+      var str=JSON.stringify(data);
+      var obj=JSON.parse(str);//객체기 때문에 이 시점부터는 요소들
+      //.쩜찍고 접근이 가능하다...
+      alert(obj.msg);
+
     });
   }
 }
